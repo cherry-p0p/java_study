2dd40d477aadfc8e855186cddf69faed91fee842
cherry-p0p <cherrypop3678@gmail.com>
2023-10-12 20:00:17 +0900

    chapter_06-1_객체 지향 프로그래밍

--- src/main/java/chapter_06/sec01/Student.java
+++ src/main/java/chapter_06/sec01/Student.java
@@ -1,4 +1,5 @@
 package chapter_06.sec01;
 
+// 라이브러리 클래스
 public class Student {
 }
--- src/main/java/chapter_06/sec01/StudentExample.java
+++ src/main/java/chapter_06/sec01/StudentExample.java
@@ -1,8 +1,14 @@
 package chapter_06.sec01;
 
+// 실행 클래스
 public class StudentExample {
     public static void main(String[] args) {
         Student s1 = new Student();
         System.out.println("s1 변수가 Student 객체를 참조합니다.");
+
+        // Student 클래스는 하나지만 new 연산자를 사용한 만큼 객체가 메모리에 생성
+        // 이러한 객체들은 Student의 인스턴
+        Student s2 = new Student();
+        System.out.println("s1 변수가 또 다른 Student 객체를 참조합니다.");
     }
 }
